7ae54e3ce90f2d2b59915b0e49100bd4aef73478
Thimmaraju <thimmaraju418@gmail.com>
2018-11-20 12:57:13 +0530

    Merging

---
+++ src/ZoomInZoomOutInSelenium.java
@@ -0,0 +1,40 @@
+import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ZoomInZoomOutInSelenium{
+	
+
+	WebDriver driver;
+
+	@Test
+	public void zoomInZoomOutInSelenium() throws InterruptedException {
+		// First step set the driver location
+		// For Window user
+		// System.setProperty("webdriver.chrome.driver", "/Users/bsingh5/Documents/coreJava/selenium/drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\WinWire\\eclipse-workspace\\Selenium_basics\\Drivers\\chromedriver.exe");	
+		//System.setProperty("webdriver.chrome.driver", "/Users/bsingh5/Documents/coreJava/selenium/drivers/chromedriver");
+		// create driver object	
+		driver = new ChromeDriver();
+		
+		driver.get("http://automationpractice.com/index.php");
+		// zoom out by 40%
+		((JavascriptExecutor) driver).executeScript("document.body.style.zoom='40%'");
+		
+		Thread.sleep(3000);
+		// zoom out by 100%
+		((JavascriptExecutor) driver).executeScript("document.body.style.zoom='70%'");
+		
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		WebElement newsletter = driver.findElement(By.xpath("//*[@id=\"index\"]"));
+		jse.executeScript("arguments[0].scrollIntoView(true);",newsletter);
+		
+	}
+}
---
+++ src/calender.java
@@ -0,0 +1,59 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class calender  {
+
+	WebDriver driver;
+
+	@Test
+	public void enterText() throws InterruptedException{
+
+		String month = "September 2019";
+		String day = "20";
+
+		// First step set the driver location
+		// For Window user
+		// System.setProperty("webdriver.chrome.driver",
+		// "/Users/bsingh5/Documents/coreJava/selenium/drivers/chromedriver.exe");
+
+		//System.setProperty("webdriver.chrome.driver",
+		//		"/Users/bsingh5/Documents/coreJava/selenium/drivers/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\WinWire\\eclipse-workspace\\Selenium_basics\\Drivers\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.get("http://www.phptravels.net/");
+
+		driver.findElement(By.xpath("//*[@id=\"dpd1\"]/div/input")).click();
+		
+			Thread.sleep(2000);
+		
+		while (true) {
+			String text = driver
+					.findElement(By.xpath("/html/body/div[9]/div[1]/table/thead/tr[1]/th[2]"))
+					.getText();
+			System.out.println(text);
+			if (text.equals(month)) {
+				break;
+			} else {
+				driver.findElement(By.xpath("//div[@class='datepicker dropdown-menu'][1]/div/table/thead/tr/th[3]"))
+						.click();
+			}
+		}
+
+		driver.findElement(
+				By.xpath("//div[@class='datepicker dropdown-menu'][1]/div/table/tbody/tr/td[contains(text(),'21')]"))
+				.click();
+		
+	driver.close();
+		//Thread.sleep(4000);
+		//driver.quit();
+		
+
+	}
+	
+}
